5e23f7760d4ba7ca38a690a40866cce02eecd3bc
proggeler <proggeler@de-mes.nl>
2023-03-09 21:14:31 +0100

    small change

--- tests/Fixtures/DummyServiceProvider.php
+++ tests/Fixtures/DummyServiceProvider.php
@@ -10,6 +10,6 @@ class DummyServiceProvider implements ServiceProviderInterface
     public function register(Container $container): void
     {
         $container->set(DummyContainer::class, fn() => new DummyContainer());
-        $container->set(DummyAdapter::class, fn() => new DummyAdapter($this->get(DummyContainer::class)));
+        $container->set(DummyAdapter::class, fn() => new DummyAdapter($container->get(DummyContainer::class)));
     }
 }
